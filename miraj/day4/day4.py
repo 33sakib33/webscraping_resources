d4efacaad7b7c03edff01b25df04c1c169f626e1
MonayemSarker <bsse1228@iit.du.ac.bd>
2025-04-28 22:42:16 +0600

    day4

---
+++ miraj/day4/day4.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+import csv
+from selenium import webdriver
+import time
+
+
+driver = webdriver.Chrome()
+
+url = "https://quotes.toscrape.com/scroll"
+driver.get(url)
+time.sleep(3)
+
+soup = BeautifulSoup(driver.page_source, "html.parser")
+
+quotes = soup.find_all("div", class_="quote")
+
+life_quotes = []
+
+for quote in quotes:
+    tags = [tag.get_text(strip=True) for tag in quote.find_all("a", class_="tag")]
+    if "life" in tags:
+        life_quotes.append(quote)
+
+with open('life_quotes_day3.csv', mode='w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(['sl', 'quote', 'by whom', 'tags'])
+
+    for idx, quote in enumerate(life_quotes[:10], start=1):
+        text = quote.find("span", class_="text").get_text(strip=True)
+        author = quote.find("small", class_="author").get_text(strip=True)
+        tags = [tag.get_text(strip=True) for tag in quote.find_all("a", class_="tag")]
+        tags_string = ", ".join(tags)
+
+        writer.writerow([idx, text, author, tags_string])
+
+print("Done")
